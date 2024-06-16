0bd267df366237262e8cd2ceb01ad454f4a3fd28
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-16 00:49:46 +0000

    serverless 4.1.4

--- Formula/s/serverless.rb
+++ Formula/s/serverless.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://www.serverless.com/"
-  url "https://github.com/serverless/serverless/archive/refs/tags/v3.39.0.tar.gz"
-  sha256 "8f9f90af64b4ddf9df872b6a998ce943d82a479d0f138f804a0e84d4f24b74e3"
+  url "https://github.com/serverless/serverless/archive/refs/tags/v4.1.4.tar.gz"
+  sha256 "00f52ddd4adc34a8719c6048d13e94696dc1bdc6b924fa9822d388d15e2fafc5"
   license "MIT"
   head "https://github.com/serverless/serverless.git", branch: "main"
 
